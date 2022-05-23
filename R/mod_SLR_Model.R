7f7f06192f4e8c790f6e4baaf7124c9a5a995696
Xuemao Zhang <43715985+xuemaozhang@users.noreply.github.com>
2022-05-23 11:41:35 -0400

    Update mod_SLR_Model.R
    update icon("clipboard", class = "btn btn-success") to icon=icon("clipboard", class = "btn btn-success")

--- R/mod_SLR_Model.R
+++ R/mod_SLR_Model.R
@@ -343,7 +343,7 @@ observe({
                                 collapse = "\n"))
 }) 
 output$clip <- renderUI({
-  rclipButton("clipbtn", "Copy-R-code-to-clipboard", input$code_slr, icon("clipboard", class = "btn btn-success"))
+  rclipButton("clipbtn", "Copy-R-code-to-clipboard", input$code_slr, icon=icon("clipboard", class = "btn btn-success"))
 })
  
    
