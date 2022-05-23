97677f12ec111ae57a0373bd464d6f31e4b39870
Xuemao Zhang <43715985+xuemaozhang@users.noreply.github.com>
2022-05-23 11:42:03 -0400

    Update mod_One_Way.R
    update icon("clipboard", class = "btn btn-success") to icon=icon("clipboard", class = "btn btn-success")

--- R/mod_One_Way.R
+++ R/mod_One_Way.R
@@ -441,7 +441,7 @@ observe({
           value = paste(code(), collapse = "\n"))
 }) 
 output$clip <- renderUI({
-  rclipButton("clipbtn", "Copy-R-code-to-clipboard", input$code_oneway, icon("clipboard", class = "btn btn-success"))
+  rclipButton("clipbtn", "Copy-R-code-to-clipboard", input$code_oneway, icon=icon("clipboard", class = "btn btn-success"))
 })
 
   })
